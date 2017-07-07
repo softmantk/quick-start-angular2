cffa79d5ee83e478683c76fa74c3717c80892277
nikhilcm <rose12nikhl@gmail.com>
2017-07-07 08:30:02 +0530

    k

---
+++ APM - Start/app/app.component.ts
@@ -0,0 +1,8 @@
+import {Component} from '@angular/core';
+@Component({
+    selector : 'pm-app',
+    templateUrl: 'app/app.component.html'
+})
+export class AppComponent {
+    pageTitle: string = 'Awero Product management system';
+}
